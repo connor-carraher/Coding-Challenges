0a8c04ccea5fb07812d590c50aa4696bb89a3b0b
connor-carraher <connor@carraher.com>
2017-11-20 02:07:29 -0800

    Greatly improved Problem 12 runtime by using prime factorization.

--- Problem12.cpp
+++ Problem12.cpp
@@ -1,28 +1,60 @@
 //Highly divisible triangular number Problem 12
 
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-int main () {
-	long triNum = 0; //Has to be greater than 500 
-	int counter = 0;
-	for(int i = 1; i < 1000000; i++){
-		triNum += i;
+vector<int> primeFactors(int n){
+    vector<int> primeFac;
 
-		for(int j = 1; j <= triNum; j++){
-			if(triNum % j == 0){
-				counter++;
-			}
-		}
-		if(counter > 500){
-			cout << triNum << endl	;
-			return 0;
-		}
-		if(counter > 100){
-			cout << i << ":  " << triNum << ":  " << counter << endl;
-		}
-		counter = 0;
-	}
+    while (n%2 == 0){
+        primeFac.push_back(2);
+        n = n/2;
+    }
+ 
 
-	return 0;
+    for (int i = 3; i <= sqrt(n); i = i+2){
+        while (n%i == 0){
+            primeFac.push_back(i);
+            n = n/i;
+        }
+    }
+ 
+    if (n > 2)
+        primeFac.push_back(n);
+
+    return primeFac;
+}
+
+int main(){
+    int triNum = 0; 
+    int divisors;
+    for(int i = 1; i < 20000; i++){
+        divisors = 1;
+        triNum += i;
+        vector<int> primeFac = primeFactors(triNum);
+        vector<int> divCount;
+
+        while(primeFac.size() != 0){
+            int counter = 1;
+            while(primeFac.size() > 1 && primeFac.back() == primeFac[primeFac.size() - 2]){
+                counter++;
+                primeFac.pop_back();
+            }
+            divCount.push_back(counter);
+            primeFac.pop_back();
+        }
+
+        while(divCount.size() != 0){
+            divisors = divisors * (1 + divCount.back());
+            divCount.pop_back();
+        }
+
+        if(divisors > 500){
+            cout << "divisors exceeded 500: " << triNum << endl;
+            return 0;
+        }        
+    }  
+    return 0;
 }
