54063d6d6ad927120c49727a39fb26ea55e5a13e
connor-carraher <connor@carraher.com>
2017-11-20 10:12:23 -0800

    Problem 19: Counting Sundays

--- Problem19.cs
+++ Problem19.cs
@@ -4,13 +4,13 @@ using System;
 using System.Diagnostics;
 using System.Numerics;
 
-class Problem19 {   
-    public static void Main() {    
+class Problem19{   
+    public static void Main(){    
         int sundays = 0;
 
-        for (int year = 1901; year <= 2000; year++) {
-            for (int month = 1; month <= 12; month++) {
-                if ((new DateTime(year, month, 1)).DayOfWeek == DayOfWeek.Sunday) {
+        for (int year = 1901; year <= 2000; year++){
+            for (int month = 1; month <= 12; month++){
+                if ((new DateTime(year, month, 1)).DayOfWeek == DayOfWeek.Sunday){
                     sundays++;
                 }
             }
